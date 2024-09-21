863636a4be2a612e17cd6ec42f4baa50350d896d
ercilli <eric.tarelli@gmail.com>
2024-09-21 20:37:17 -0300

    refactor: Add Header component for improved website navigation

---
+++ Components/Header.js
@@ -0,0 +1,17 @@
+export default {
+    template: `
+      <header>
+        <nav>
+            <div class="logo">Vivero Ganesha</div>
+            <div class="menu">
+                <ul>
+                    <li><a href="/Pages/facturacion.html" class="about-button">Facturacion</a></li>
+                    <li><a href="/Pages/stock.html" class="about-button">Inventario</a></li>
+                    <li><a href="/index.html" class="about-button">Producto</a></li>
+                    <li><a href="/Pages/venta.html" class="menu-button">Venta</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    `
+  };
