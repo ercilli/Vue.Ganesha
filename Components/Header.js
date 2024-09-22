d8fbfb2af3e28eb201f15f73b50cef99d4e24ada
ercilli <eric.tarelli@gmail.com>
2024-09-22 16:48:38 -0300

    feat: Add "Proveedor" and "Cliente" links to the navigation menu

--- Components/Header.js
+++ Components/Header.js
@@ -8,6 +8,8 @@ export default {
                     <li><a href="./facturacion.html" class="about-button">Facturacion</a></li>
                     <li><a href="./stock.html" class="about-button">Inventario</a></li>
                     <li><a href="./index.html" class="about-button">Producto</a></li>
+                    <li><a href="./proveedor.html" class="about-button">Proveedor</a></li>
+                    <li><a href="./cliente.html" class="about-button">Cliente</a></li>
                     <li><a href="./venta.html" class="menu-button">Venta</a></li>
                 </ul>
             </div>
