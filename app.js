50ed6a0007c0e53faa9f66c58d60d949aed9c053
ercilli <eric.tarelli@gmail.com>
2024-09-22 19:39:06 -0300

    refactor: Update app.js to add modal functionality and fix component names

--- app.js
+++ app.js
@@ -2,18 +2,32 @@ import Producto from './Components/Producto.js';
 import Stock from './Components/Stock.js';
 import Venta from './Components/Venta.js';
 import Facturacion from './Components/Facturacion.js';
-import { fetchItems } from './Services/apiService.js'; 
 import Footer from './Components/Footer.js';
 import Header from './Components/Header.js';
 import Cliente from './Components/Cliente.js';
 import Proveedor from './Components/Proveedor.js';
-
+import Modal from './Components/Modal.js'; // Asegúrate de que la ruta sea correcta
 
 const app = Vue.createApp({
   data() {
     return {
-      products: []
+      products: [],
+      showModal: false,
+      modalTitle: '',
+      modalMessage: '',
+      modalType: ''
     };
+  },
+  methods: {
+    openModal(title, message, type) {
+      this.modalTitle = title;
+      this.modalMessage = message;
+      this.modalType = type;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    }
   },
   components: {
     'producto': Producto,
@@ -23,13 +37,8 @@ const app = Vue.createApp({
     'app-footer': Footer,
     'app-header': Header,
     'cliente': Cliente,
-    'proveedor': Proveedor
-  },
-  mounted() {
-    // Use fetchItems with the 'Producto' controller to fetch products
-    fetchItems('Producto').then(data => {
-      this.products = data;
-    }).catch(error => console.error('Error al cargar los productos:', error));
+    'proveedor': Proveedor,
+    'modal': Modal // Asegúrate de que el nombre del componente esté en minúsculas
   }
 });
 
