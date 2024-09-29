26aa90b51b92bfeeb493064aad2ed2c109f75195
ercilli <eric.tarelli@gmail.com>
2024-09-29 16:17:33 -0300

    refactor: Update header component to use semantic HTML and improve navigation
    - Replace the <div> elements with appropriate semantic HTML tags in the header component.
    - Improve the navigation menu by adding a hamburger menu icon and making it responsive.
    - Update the links in the navigation menu to have consistent styling.
    
    Closes #123

--- Components/Header.js
+++ Components/Header.js
@@ -1,19 +1,48 @@
 export default {
-    template: `
-      <header>
-        <nav>
-            <div class="logo">Vivero Ganesha</div>
-            <div class="menu">
-                <ul>
-                    <li><a href="./facturacion.html" class="about-button">Facturacion</a></li>
-                    <li><a href="./stock.html" class="about-button">Inventario</a></li>
-                    <li><a href="./index.html" class="about-button">Producto</a></li>
-                    <li><a href="./proveedor.html" class="about-button">Proveedor</a></li>
-                    <li><a href="./cliente.html" class="about-button">Cliente</a></li>
-                    <li><a href="./venta.html" class="menu-button">Venta</a></li>
-                </ul>
-            </div>
-        </nav>
+  template: `
+    <header class="header">
+      <nav class="navbar">
+        <h1 class="logo">Vivero Ganesha</h1>
+
+        <label class="labe_hamburguesa" for="menu_hamburguesa">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            fill="currentColor"
+            class="list_icon"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </label>
+
+        <input class="menu_hamburguesa" type="checkbox" name="" id="menu_hamburguesa">
+
+        <ul class="ul_links">
+          <li class="li_links">
+            <a href="./facturacion.html" class="about-button">Facturación</a>
+          </li>
+          <li class="li_links">
+            <a href="./stock.html" class="about-button">Inventario</a>
+          </li>
+          <li class="li_links">
+            <a href="./index.html" class="about-button">Producto</a>
+          </li>
+          <li class="li_links">
+            <a href="./proveedor.html" class="about-button">Proveedor</a>
+          </li>
+          <li class="li_links">
+            <a href="./cliente.html" class="about-button">Cliente</a>
+          </li>
+          <li class="li_links">
+            <a href="./venta.html" class="about-button">Venta</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-    `
-  };
+  `
+};
