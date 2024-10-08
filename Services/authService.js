6a7f262095aa7a532eff8d902ecbc2585b286eeb
ercilli <eric.tarelli@gmail.com>
2024-10-08 02:35:41 -0300

    Refactor authService.js to use async/await for fetching items, getting item by ID, creating item, updating item, and deleting item

--- Services/authService.js
+++ Services/authService.js
@@ -4,10 +4,14 @@ import { handleError } from '../Components/ErrorHandler.js';
 const baseUrl = 'https://vivero-ganesha-api.fly.dev/api';
 
 let accessToken = localStorage.getItem('accessToken');
+let refreshToken = localStorage.getItem('refreshToken');
 let tokenExpiry = localStorage.getItem('tokenExpiry');
 
 // Función para decodificar el token JWT
 const decodeToken = (token) => {
+  if (typeof token !== 'string') {
+    throw new TypeError('El token debe ser una cadena');
+  }
   const payload = token.split('.')[1];
   const decodedPayload = atob(payload);
   return JSON.parse(decodedPayload);
@@ -25,10 +29,11 @@ export const loginUser = async (credentials) => {
 
     if (response.status === 200) {
       const data = await response.json();
-      accessToken = data.token;
-      const decodedToken = decodeToken(accessToken);
-      tokenExpiry = decodedToken.exp * 1000; // Convertir a milisegundos
+      accessToken = data.accessToken;
+      refreshToken = data.refreshToken;
+      tokenExpiry = new Date().getTime() + data.expiresIn * 1000; // Convertir a milisegundos
       localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
       localStorage.setItem('tokenExpiry', tokenExpiry);
       return data;
     } else {
@@ -41,20 +46,19 @@ export const loginUser = async (credentials) => {
 };
 
 // Función para registrar un nuevo usuario
-export const registerUser = async (userDetails) => {
+export const registerUser = async (userData) => {
   try {
-    console.log('Registering user:', userDetails);
-    const response = await fetch(`${baseUrl}/login/register`, {
+    const response = await fetch(`${baseUrl}/login/signup`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(userDetails)
+      body: JSON.stringify(userData)
     });
 
-    if (response.status === 200) {
+    if (response.status === 201 || response.status === 200) {
       const data = await response.json();
       return data;
     } else {
-      throw new Error('Error al registrar usuario');
+      throw new Error('Error al registrar el usuario');
     }
   } catch (error) {
     handleError(error, 'registrar usuario');
@@ -62,13 +66,42 @@ export const registerUser = async (userDetails) => {
   }
 };
 
+// Función para refrescar el token
+const refreshAccessToken = async () => {
+  try {
+    const response = await fetch(`${baseUrl}/login/refresh`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ refreshToken })
+    });
+
+    if (response.status === 200) {
+      const data = await response.json();
+      accessToken = data.accessToken;
+      refreshToken = data.refreshToken;
+      tokenExpiry = new Date().getTime() + data.expiresIn * 1000; // Convertir a milisegundos
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
+      localStorage.setItem('tokenExpiry', tokenExpiry);
+      return accessToken;
+    } else {
+      throw new Error('Error al refrescar el token');
+    }
+  } catch (error) {
+    handleError(error, 'refrescar token');
+    throw error;
+  }
+};
+
 // Función para obtener el token actual
-export const getAccessToken = () => {
+export const getAccessToken = async () => {
   try {
     if (new Date().getTime() > tokenExpiry) {
-      // Token ha expirado
-      logoutUser();
-      throw new Error('Token ha expirado. Por favor, inicie sesión de nuevo.');
+      // Token ha expirado, intentar refrescarlo
+      return await refreshAccessToken();
+    }
+    if (!accessToken) {
+      throw new Error('No hay un token de acceso disponible');
     }
     return accessToken;
   } catch (error) {
@@ -78,11 +111,12 @@ export const getAccessToken = () => {
 };
 
 // Función para obtener el rol del usuario
-export const getUserRole = () => {
+export const getUserRole = async () => {
   try {
-    const token = getAccessToken();
-    if (!token) return null;
-
+    const token = await getAccessToken();
+    if (!token) {
+      throw new Error('No se pudo obtener el token de acceso');
+    }
     const decodedToken = decodeToken(token);
     return decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']; // Usar el URI correcto para obtener el rol
   } catch (error) {
@@ -94,7 +128,9 @@ export const getUserRole = () => {
 // Función para cerrar sesión
 export const logoutUser = () => {
   accessToken = null;
+  refreshToken = null;
   tokenExpiry = null;
   localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
   localStorage.removeItem('tokenExpiry');
 };
