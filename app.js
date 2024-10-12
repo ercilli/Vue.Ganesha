555ea5e5abf343ebf33856b899e61edaa54a025d
ercilli <eric.tarelli@gmail.com>
2024-10-12 18:05:39 -0300

    Refactor app.js to remove unused import and modal component

--- app.js
+++ app.js
@@ -6,32 +6,11 @@ import Footer from './Components/Footer.js';
 import Header from './Components/Header.js';
 import Cliente from './Components/Cliente.js';
 import Proveedor from './Components/Proveedor.js';
-import MyModal from './Components/MyModal.js'; // Asegúrate de que la ruta sea correcta
 import Login from './Components/Login.js';
 import User from './Components/User.js';
 import DescuentoCantidad from './Components/DescuentoCantidad.js';
 
 const app = Vue.createApp({
-  data() {
-    return {
-      products: [],
-      showModal: false,
-      modalTitle: '',
-      modalMessage: '',
-      modalType: ''
-    };
-  },
-  methods: {
-    openModal(title, message, type) {
-      this.modalTitle = title;
-      this.modalMessage = message;
-      this.modalType = type;
-      this.showModal = true;
-    },
-    closeModal() {
-      this.showModal = false;
-    }
-  },
   components: {
     'descuento-cantidad': DescuentoCantidad,
     'user': User,
@@ -44,7 +23,6 @@ const app = Vue.createApp({
     'app-header': Header,
     'cliente': Cliente,
     'proveedor': Proveedor,
-    'modal': MyModal // Asegúrate de que el nombre del componente esté en minúsculas
   }
 });
 
